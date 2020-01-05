b704218224b86b8c32bf33c49adc0fcd85900f75
Michael Viveros <michaelviveros@gmail.com>
2020-01-05 16:23:33 -0500

    refactor code for 2020 workshop

--- pairs.py
+++ pairs.py
@@ -2,79 +2,65 @@
 # 
 # Input: a = [1, 1, 3], sum = 4
 # Output: [1, 3]
-# 
-# Input: a = [ 7, 2, 5, 8, 3 ], sum = 10
+
+# Input: a = [2, 3, 4, 7, 8], sum = 10
 # Output: [3, 7], [2, 8]
 
-# a = [None, 1, None]
+# first  simple/slow version we got working
+def pairs_sum_to_x_1(arr, x):
+	print(arr)
+	pairs = []
+	# time - O(n^2), space - O(n)
+	for i in range(len(arr)): # O(n)
+		numI = arr[i]
+		for j in range(len(arr)): # O(n)
+			numJ = arr[j]
+			if numI != None and numJ != None and numI + numJ == x:
+				print(f"found pair - i {i}, j {j}, numI {numI}, numJ {numJ}")
+				pairs.append([numI, numJ])
+				arr[i] = None
+				arr[j] = None
+	return pairs
 
-# numI + numJ = targetSum
-# numJ = targetSum - numI
-# numI = 1, numJ = 4 - 1 = 3
+# refactored first version to show that inner loop is essentially looking for a number in an array	
+def pairs_sum_to_x_2(arr, x):
+	print(arr)
+	pairs = []
+	# time - O(n^2), space - O(n)
+	for i in range(len(arr)): # O(n)
+		numI = arr[i]
+		if numI == None:
+			continue
+		targetJ = x - numI
+		for j in range(len(arr)): # O(n)
+			numJ = arr[j]
+			if numI != None and numJ != None and numJ == targetJ:
+				print(f"found pair - i {i}, j {j}, numI {numI}, numJ {numJ}")
+				pairs.append([numI, numJ])
+				arr[i] = None
+				arr[j] = None
+	return pairs
 
-# searching a list - O(n)
-# searching a hash map - O(1)
-# { 1 => 2, 3 => 1 }
-# numI = 1, numJ = 4 - 1 = 3, 
-# 3 in hash map? yes!
-# pair [1, 3]
-# { 1 => 1, 3 => 0 }
+# final optimal version where we use a dictionary
+def pairs_sum_to_x_3(arr, x):
+	occurrences = {}
+	for num in arr:
+		if num not in occurrences:
+			occurrences[num] = 1
+		else:
+			occurrences[num] += 1
+	print(f"occurrences {occurrences}")
 
-# time - O(n), space - O(n)
-def pairsSumToTarget(nums, targetSum):
-    occurrences = {} # space - O(n)
+	pairs = []
+	# time - O(n), space - O(n)
+	for i in range(len(arr)): # O(n)
+		numI = arr[i]
+		targetJ = x - numI
+		# O(1)
+		if targetJ in occurrences and occurrences[targetJ] >= 1 and occurrences[numI] >= 1:
+			pairs.append([numI, targetJ])
+			occurrences[numI] -= 1
+			occurrences[targetJ] -= 1
+	return pairs
 
-    # time - O(n)
-    for num in nums:
-        if num in occurrences:
-            occurrences[num] += 1
-        else:
-            occurrences[num] = 1
-    print("occurrences " + str(occurrences))
-
-    pairs = [] # space - O(n)
-    # time - O(n * 1)
-    for i in range(0, len(nums)): # time - O(n)
-        numI = nums[i]
-        if occurrences[numI] == 0:
-            continue
-        numJ = targetSum - numI
-        if numI == numJ:
-            if occurrences[numI] >= 2: # time - O(1)
-                print("numI " + str(numI) + ", numJ " + str(numJ))
-                pairs.append([numI, numJ])
-                occurrences[numI] -= 2
-                print("occurrences " + str(occurrences))
-        else:
-            if numJ in occurrences and occurrences[numJ] >= 1: # time - O(1)
-                print("numI " + str(numI) + ", numJ " + str(numJ))
-                pairs.append([numI, numJ])
-                occurrences[numI] -= 1
-                occurrences[numJ] -= 1
-                print("occurrences " + str(occurrences))
-    print("\n pairs " + str(pairs))
-
-print("pairsSumToTarget([ 1, 1, 3 ], 4):\n")
-pairsSumToTarget([ 1, 1, 3 ], 4)
-
-print("\n\npairsSumToTarget([ 7, 2, 5, 8, 3 ], 10):\n")
-pairsSumToTarget([ 7, 2, 5, 8, 3 ], 10)
-
-# time - O(n^2), space - O(n)
-def pairsSumToTargetSlow(nums, targetSum):
-    pairs = [] # space - O(n)
-    # time - O(n * n) = O(n^2)
-    for i in range(0, len(nums)): # time - O(n)
-        numI = nums[i]
-        if numI == None:
-            continue
-        numJ = targetSum - numI
-        for j in range(i + 1, len(nums)): # time - O(n)
-            if nums[i] != None and nums[j] != None:
-                if nums[j] == numJ:
-                    print("i " + str(i) + ", j " + str(j))
-                    pairs.append([nums[i], nums[j]])
-                    nums[i] = None
-                    nums[j] = None
-                    print("nums " + str(nums))
-        print("\n pairs " + str(pairs))
+print(pairs_sum_to_x_3([2, 3, 4, 7, 8], 10))
